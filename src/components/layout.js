c08f3e940ac3cdf3421f83d36a0bd20bfdc00ff4
Sebastian <sebgertz@gmail.com>
2019-06-04 12:35:08 -0400

    update layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,13 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
+import Sidebar from "./Sidebar/sidebar"
+import "../assets/sass/layout.scss"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
-import Header from "./header"
-import "./layout.css"
+library.add(fas, fab)
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -24,24 +28,14 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
+      <div style={{display: 'flex'}}>
+        <Sidebar siteTitle={data.site.siteMetadata.title}
+        />
+        <div className="container">
+          <main style={{ flex: 1 }}>{children}</main>
+          <footer>Yoohoo</footer>
         </div>
-      </>
+      </div>
     )}
   />
 )
