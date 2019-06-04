412caf5fe7ebd5ee3625c3fce55989554dd154e7
Sebastian <sebgertz@gmail.com>
2019-06-04 12:37:50 -0400

    add sidebar footer

---
+++ src/components/Sidebar/footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default class Footer extends React.Component {
+  render() {
+    const socialLinks = this.props.socialLinks
+    const socialLinksMarkup = socialLinks.map(
+      link => {
+        return <li key={link.id}>
+          <a href={link.url} target='_blank'>
+            <FontAwesomeIcon icon={['fab', link.id]} style={{color: 'black', fontSize: '18px'}}/>
+          </a>
+        </li>
+      }
+     )
+    return (
+      <div style={{flex: 0.5}} className="footer-wrapper">
+        <ul className="icons">
+          {socialLinksMarkup}
+        </ul>
+    </div>
+    )
+  }
+}
