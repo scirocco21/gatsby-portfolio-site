65362e9d7a8725bf405df18c2eb8ecc0bf9f9a6f
Sebastian <sebgertz@gmail.com>
2019-06-04 12:38:24 -0400

    add config.js with social media links

---
+++ src/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  socialLinks: [
+    {
+      id: 'github',
+      name: 'Github',
+      url: 'https://github.com/scirocco21',
+    },
+    {
+      id: 'linkedin-in',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/sebastian-gertz/',
+    },
+    {
+      id: 'twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/cryptocyclops',
+    }
+  ]
+}
