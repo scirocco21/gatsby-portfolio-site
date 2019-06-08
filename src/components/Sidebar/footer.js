7d89156c38241c0e0f0e8e14e66ad2230af5eb68
Sebastian <sebgertz@gmail.com>
2019-06-07 21:40:45 -0400

    update footer component to not receive props and import config.js instead

--- src/components/Sidebar/footer.js
+++ src/components/Sidebar/footer.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import config from '../../config.js';
+
 
 export default class Footer extends React.Component {
   render() {
-    const socialLinks = this.props.socialLinks
+    const socialLinks = config.socialLinks
     const socialLinksMarkup = socialLinks.map(
       link => {
         return <li key={link.id}>
-          <a href={link.url} target='_blank' rel='noopener noreferrer'>
-            <FontAwesomeIcon icon={['fab', link.id]} style={{color: 'black', fontSize: '18px'}}/>
+          <a href={link.url} target='_blank' rel='noopener noreferrer' style={{color: '#808080'}}>
+            <FontAwesomeIcon icon={['fab', link.id]} style={{fontSize: '24px'}}/>
           </a>
         </li>
       }
