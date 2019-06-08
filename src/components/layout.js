73c6ad4515d0f795405c1492faa6112daec07dc9
Sebastian <sebgertz@gmail.com>
2019-06-07 21:42:37 -0400

    update footer in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,7 @@ import "../assets/sass/layout.scss"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import Footer from './Sidebar/footer.js'
 
 library.add(fas, fab)
 
@@ -33,7 +34,12 @@ const Layout = ({ children }) => (
         />
         <div className="container">
           <main style={{ flex: 1 }}>{children}</main>
-          <footer>Yoohoo</footer>
+            <footer className='layout-footer' style={{ color: '#808080'}}>
+              <p style={{padding: 10, fontWeight: 'bold'}}>&#169;
+                2019 Sebastian Gertz</p>
+              <p>Made with <a style={{color: '#fffff0'}} href="https://www.gatsbyjs.org/">Gatsby.JS</a></p>
+              <Footer></Footer>
+            </footer>
         </div>
       </div>
     )}
