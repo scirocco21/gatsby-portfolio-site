183e36a1cc9e5f744dc6efcebb66fda5c060b3fd
scirocco21 <sebgertz@gmail.com>
2019-06-26 17:51:44 -0400

    chore: clean up landing page design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Layout from "../components/layout";
 import BackgroundSection from '../components/backgroundImageSection';
+import "../assets/sass/index.scss"
+
 import SEO from "../components/seo";
 
 const IndexPage = () => (
@@ -19,14 +21,15 @@ const IndexPage = () => (
           flexDirection: 'column', 
           alignItems: 'center', 
           marginBottom: 20, 
-          width: '80%',
+          width: '70%',
           borderRadius: '79% 20% 59% 18% / 17% 72% 21% 64%'
-        }}>
-          <div style={{padding: 25, fontSize: '3vw'}}>
-            <h1 style={{fontSize: '6vw', marginTop: 10, padding: 10}}>Hi, I'm <mark style={{backgroundColor: '#F4C402', color: '#240447'}}>Sebastian</mark></h1>
-            <h3>I'm a web developer living in NYC.</h3>
-            <h3>Glad you found me! 🙌 👍 👏 </h3>
-            <h3 style={{marginBottom: 15}}>👈 Check out my projects and posts</h3>
+        }} id="landing-container" className="roll-in-blurred-left">
+          <div style={{ fontSize: '1em', padding: 15}}>
+            <h1 style={{fontSize: '3em', paddingTop: 20, paddingRight: 70, paddingLeft: 70}}><span>Hi, I'm</span><br></br>
+              <mark style={{backgroundColor: '#EE015D', color: '#240447'}} className="tracking-in-expand">Sebastian</mark></h1>
+            <h3>I'm a web developer living in NYC. </h3>
+            <h3>Glad you found me! <span id="highFive" style={{marginLeft: 10, marginRight: 10, textShadow: '10px 10px 10px'}}>🙌</span></h3>
+            <h3 style={{paddingBottom: '15px'}}><span id="pointer" style={{textShadow: '10px 10px 10px', paddingLeft: '25px', paddingRight: '10px'}}>👈</span> Check out my projects and posts</h3>
           </div>
         </div>
       </BackgroundSection>
