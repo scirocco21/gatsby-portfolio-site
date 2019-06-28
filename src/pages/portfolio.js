8d0cb6df576f16f734b0c6617e8e641417912430
scirocco21 <sebgertz@gmail.com>
2019-06-28 10:55:57 -0400

    add image carousel to portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,15 +1,59 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from "../components/seo";
+import { Carousel } from 'react-responsive-carousel';
+import myReads from '../images/myReads.jpg';
+import coinopoly from '../images/coinopoly.jpg';
+import voynich from '../images/voynich.jpg';
+import biblio from '../images/biblio.jpg';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { graphql, StaticQuery } from 'gatsby'
+import Img from "gatsby-image"
+
 
 const Portfolio = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/index">Go back to the homepage</Link>
+    <SEO title="Portfolio" />
+    <StaticQuery query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "museums-explorer.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 4160) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `}
+     render={data => {
+       const imageData = data.desktop.childImageSharp.fluid
+       return (
+        <Carousel autoPlay={true} infiniteLoop={true} showThumbs={false}>
+          <div>
+            <Img fluid={imageData} />
+            <p className="legend">Museums Explorer</p>
+          </div>
+          <div>
+            <img src={myReads} />
+            <p className="legend">Personal Bookshelf Organiser</p>
+          </div>
+          <div>
+            <img src={coinopoly} />
+            <p className="legend">Cryptocurrency Portfolio Tracker</p>
+          </div>
+          <div>
+            <img src={voynich} />
+            <p className="legend">Matching Game</p>
+          </div>  
+          <div>
+            <img src={biblio} />
+            <p className="legend">Booknotes App</p>
+          </div>
+        </Carousel>
+       )
+      }
+     }
+     />
   </Layout>
 )
 
