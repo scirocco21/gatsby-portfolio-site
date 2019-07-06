94f20e8b0d125574d323322ba9494d5f03d76353
scirocco21 <sebgertz@gmail.com>
2019-07-06 12:33:55 -0400

    add blog post template

---
+++ src/templates/blog.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Layout from "../components/layout";
+import { graphql,} from "gatsby";
+import Img from "gatsby-image";
+import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+
+import {documentToReactComponents} from '@contentful/rich-text-react-renderer';
+export const query = graphql`
+  query($slug: String!) {
+    contentfulBlogPost(slug: {eq: $slug}) {
+      title
+      heroImage {
+        fluid {
+        ...GatsbyContentfulFluid
+        }
+      }
+      tldr {
+        internal {
+          content
+        }
+      }
+      publishedDate(formatString: "MMMM Do, YYYY")
+      content {
+        json
+      }
+    }
+  } 
+`
+
+const Blog = (props) => {
+  const Text = ({ children }) => <p>{children}</p>;
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        const alt = node.data.target.fields.title['en-US'];
+        const url = node.data.target.fields.file['en-US'].url;
+        return <img alt={alt} src={url} style={{maxWidth: '100%',
+          height: 'auto'}}/>
+      },
+      [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+    }
+  }
+  return (
+    <Layout displayFooter={true}>
+      <div style={{padding: '20px', textAlign: 'left'}}>
+        <div>
+          <div>
+            <h1>{props.data.contentfulBlogPost.title}</h1>
+            <p>{props.data.contentfulBlogPost.publishedDate}</p>
+            {props.data.contentfulBlogPost.tldr.internal.content && <p>TLDR: <em>{props.data.contentfulBlogPost.tldr.internal.content}</em></p>}
+          </div>
+        </div>  
+          {props.data.contentfulBlogPost.heroImage && <Img fluid={props.data.contentfulBlogPost.heroImage.fluid} style={{maxWidth: '70%', height: 'auto'}}/>}
+        {documentToReactComponents(props.data.contentfulBlogPost.content.json, options)}
+      </div>
+    </Layout>
+  )
+}
+
+export default Blog
