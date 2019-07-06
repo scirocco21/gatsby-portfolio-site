b26c2b3a5d871cdddda000b65c0beca83c873c1c
scirocco21 <sebgertz@gmail.com>
2019-07-06 12:20:56 -0400

    add content to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,26 +2,48 @@ import React from "react";
 import Layout from "../components/layout";
 import BackgroundSection from '../components/backgroundImageSection';
 import "../assets/sass/index.scss"
+import config from '../config.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import SEO from "../components/seo";
 
-const Index = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div style={{position: 'relative', zIndex: 1}}>
-      <BackgroundSection>
-        <div id="overlay-main" className="fade-in">
-          <div id="text-container-main">
-            <h1 id="intro-text-main"><span>Hi, I'm</span><br></br>
-              <mark style={{backgroundColor: '#EE015D', color: '#240447', padding: '0 5px 0 5px'}} className="tracking-in-expand">Sebastian</mark></h1>
-            <h3>I'm a web developer living in NYC. </h3>
-            <h3>Glad you found me! <span id="highFive">🙌</span></h3>
-            <h3><span id="pointer">👈</span> Check out my projects and posts</h3>
-          </div>
+export default class Index extends React.Component {
+  
+  render() {
+    const socialLinks = config.socialLinks
+    const socialLinksMarkup = socialLinks.map(
+      link => {
+        return <li key={link.id}>
+          <a href={link.url} target='_blank' rel='noopener noreferrer' style={{color: '#48017C'}}>
+            <FontAwesomeIcon icon={['fab', link.id]} style={{marginRight: '1.25rem', fontSize: '2rem'}}/>
+          </a>
+        </li>
+      }
+     )
+    return(
+     <Layout displayFooter={false}>
+        <SEO title="Home" />
+        <div style={{position: 'relative', zIndex: 1}}>
+          <BackgroundSection>
+            <div id="overlay-main" className="roll-in-left fade-in">
+            
+              <div id="text-container-main">
+                <h2 id="intro-text-main" style={{ fontSize:"2.5rem", lineHeight: '1.3'}}><span>Hi, I'm</span><br></br>
+                  <span className="tracking-in-expand">Sebastian</span>
+                </h2>
+                
+                <hr></hr>
+                <ul className="icons">
+                  {socialLinksMarkup}
+                </ul>
+                <p>I'm a web developer living in NYC. </p>
+                <p>Glad you found me! <span id="highFive" style={{paddingLeft: '15px'}}>🙌</span></p>
+              </div>
+            </div>
+          </BackgroundSection>
         </div>
-      </BackgroundSection>
-    </div>
-  </Layout>
-)
-
-export default Index
+  
+      </Layout>
+    )
+  }
+}
