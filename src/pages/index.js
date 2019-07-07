2cbb55dc2961e28fa2024ba27d85e734c8c44af8
scirocco21 <sebgertz@gmail.com>
2019-07-06 21:40:10 -0400

    import Head component and set title props

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,10 @@ import Layout from "../components/layout";
 import BackgroundSection from '../components/backgroundImageSection';
 import "../assets/sass/index.scss"
 import config from '../config.js';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import SEO from "../components/seo";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Head from "../components/head";
 
 export default class Index extends React.Component {
-  
   render() {
     const socialLinks = config.socialLinks
     const socialLinksMarkup = socialLinks.map(
@@ -22,22 +20,22 @@ export default class Index extends React.Component {
      )
     return(
      <Layout displayFooter={false}>
-        <SEO title="Home" />
+        <Head title="Home" />
         <div style={{position: 'relative', zIndex: 1}}>
           <BackgroundSection>
             <div id="overlay-main" className="roll-in-left fade-in">
             
               <div id="text-container-main">
-                <h2 id="intro-text-main" style={{ fontSize:"2.5rem", lineHeight: '1.3'}}><span>Hi, I'm</span><br></br>
+                <h1 id="intro-text-main" style={{ fontSize:"2.5rem", lineHeight: '1.3'}}><span>Hi, I'm</span><br></br>
                   <span className="tracking-in-expand">Sebastian</span>
-                </h2>
+                </h1>
                 
                 <hr></hr>
                 <ul className="icons">
                   {socialLinksMarkup}
                 </ul>
-                <p>I'm a web developer living in NYC. </p>
-                <p>Glad you found me! <span id="highFive" style={{paddingLeft: '15px'}}>🙌</span></p>
+                <h4>I'm a web developer living in NYC.</h4>
+                <h4>Glad you found me! <span id="highFive" style={{paddingLeft: '15px'}}>🙌</span></h4>
               </div>
             </div>
           </BackgroundSection>
