bbfcfc6439a1e05919787a0c95ce94ea0cee92e7
scirocco21 <sebgertz@gmail.com>
2019-07-06 21:43:22 -0400

    add iframe to custom 404 page and import Head component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,22 @@
 import React from "react"
-
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Head from "../components/head"
+import "../assets/sass/404.scss";
+
+
+const Iframe = (props) => ( 
+  <div>          
+    <iframe src={props.src} height={props.height} width={props.width} frameborder={props.frameborder}/>
+  </div>
+)
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <Head title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Iframe src="https://giphy.com/embed/GK2zBA4xXqIPS" height="250px" width="250px" frameborder="0"></Iframe>
+    <p><a href="https://giphy.com/gifs/loop-space-lost-GK2zBA4xXqIPS">via GIPHY</a></p>
   </Layout>
 )
 
