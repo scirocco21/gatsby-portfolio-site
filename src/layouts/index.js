d29f532b0beb665b6ca9afc841cc5195af6c67f3
scirocco21 <sebgertz@gmail.com>
2019-07-08 16:10:43 -0400

    add landing page specific layout component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { ContextProviderComponent } from "../contexts/context.js"
 import { StaticQuery, graphql } from "gatsby"
 import Sidebar from "../components/Sidebar/sidebar"
+import SimpleLayout from './simple.js'
 import "../assets/sass/layout.scss"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
@@ -13,13 +15,15 @@ library.add(fas, fab)
 class Layout extends React.Component {
     render() {
       return(
-      <div id="main-container" style={{display: 'flex'}}>
-        <Sidebar siteTitle={this.props.title}/>
-        <div className="container">
-          <main style={{ flex: 1 }}>{this.props.children}</main>
-            <Footer></Footer>
-        </div>
-      </div>
+      <ContextProviderComponent>
+            <div id="main-container" style={{display: 'flex'}}>
+              <Sidebar siteTitle={this.props.title}/>
+              <div className="container">
+                <main style={{ flex: 1 }}>{this.props.children}</main>
+                <Footer /> 
+              </div>
+            </div>
+      </ContextProviderComponent>
     )
   }
 }
@@ -28,8 +32,8 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default (props) => (
-  <StaticQuery
+export default (props) => {
+  return <StaticQuery
     query={graphql`
       query SiteTitleQuery {
         site {
@@ -39,9 +43,13 @@ export default (props) => (
         }
       }
     `}
-    render={(data) => (
-      <Layout title={data.site.siteMetadata.title} {...props}/>
-    )}
+      render={(data) => {
+        if (props.pageResources.page.path == "/home/") {
+          return <SimpleLayout>{props.children}</SimpleLayout>
+        }
+        return <Layout title={data.site.siteMetadata.title} {...props}/>
+      }
+    }
   />
-)
+  }
 
