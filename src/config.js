9569052408c08ee88cef19edae7cbf49f254a4ee
scirocco21 <sebgertz@gmail.com>
2019-07-10 12:27:35 -0400

    add project details to site config

--- src/config.js
+++ src/config.js
@@ -15,5 +15,67 @@ module.exports = {
       name: 'Twitter',
       url: 'https://twitter.com/cryptocyclops',
     }
+  ],
+  projects: [
+    {
+      name: "NYC Museums Explorer",
+      desc: "Visualize Google Maps Data",
+      tags: [
+        "React",
+        "Google Maps API",
+        "Foursquare API"
+      ],
+      url: "https://nyc-museums-explorer.herokuapp.com",
+      img: "museums-explorer.jpg"
+    },
+    {
+      name: "Coinopoly",
+      desc: "Cryptocurrency Portfolio Tracker",
+      tags: [
+        "Ruby on Rails",
+        "React"
+      ],
+      url: "https://coinopoly.herokuapp.com",
+      img: "coinopoly.jpg"
+    },
+    {
+      name: "Biblio",
+      desc: "Booknotes Manager built with Sinatra",
+      tags: [
+        "Sinatra",
+        "Ruby"
+      ],
+      url: "https://biblio-sinatra-app.herokuapp.com/",
+      img: "biblio.jpg"
+    },
+    {
+      name: "MyReads",
+      desc: "Bookshelf Organizer built with React",
+      tags: [
+        "React"
+      ],
+      url: "https://myreads-bookshelf-app.herokuapp.com/",
+      img: "myReads.jpg"
+    },
+    // {
+    //   name: "Pixabay Viewer",
+    //   desc: "React Native Image Viewing App",
+    //   tags: [
+    //     "React Native",
+    //     "Pixabay API"
+    //   ],
+    //   url: "none yet",
+    //   img: "undefined"
+    // },
+    {
+      name: "Matching Game",
+      desc: "Voynich Manuscript Themed Matching Game",
+      tags: [
+        "JavaScript",
+        "CSS",
+      ],
+      url: "coming soon",
+      path: "../images/voynich.jpg"
+    }
   ]
 }
