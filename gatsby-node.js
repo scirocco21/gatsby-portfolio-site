a893b14bce205af7dfa18c653f7e9f227a517f4d
scirocco21 <sebgertz@gmail.com>
2019-07-10 14:26:18 -0400

    add redirect to gatsby node file

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,7 +9,10 @@
 const path = require('path')
 
 module.exports.createPages = async ({graphql, actions }) => {
-  const {createPage} = actions
+  const {createPage} = actions;
+  const { createRedirect } = actions
+  createRedirect({ fromPath: '/', toPath: '/home', isPermanent: true })
+  
   const blogTemplate = path.resolve('./src/templates/blog.js');
   const res = await graphql(`
     query {
