17eeb0a0e246a7995c6120ce0f2a688dfeee6055
scirocco21 <sebgertz@gmail.com>
2019-07-10 13:00:27 -0400

    add head info to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Head from '../components/head'
 import carbon from "../images/carbon.png";
 
 export default class About extends React.Component {
   
   render() {
     return(
+      <>
+      <Head title="About"></Head>
        <img style={{width:'100%'}} src={carbon}/>
+      </>
     )
   }
 }
