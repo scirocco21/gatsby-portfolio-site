3d68225fb2e20ad3891acdc1542055ca7d211b9b
scirocco21 <sebgertz@gmail.com>
2019-07-11 18:14:47 -0400

    refactor landing page to make use of new bg image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Head from "../components/head";
 export default class Home extends React.Component {
   
   render() {
+    const backgroundImage = "new-background.png"
     const socialLinks = config.socialLinks
     const socialLinksMarkup = socialLinks.map(
       link => {
@@ -20,19 +21,18 @@ export default class Home extends React.Component {
       }
      )
      
-
     return(
     <>
       <Head title="Home" />
       <div style={{position: 'relative', zIndex: 1}}>
-        <BackgroundSection>
+        <BackgroundSection imgName={backgroundImage}>
           <div id="overlay-main" className='roll-in-left tracking-in-expand'>
             <div id="text-container-main">
               <h1 id="intro-text-main" style={{ fontSize:"2.5rem", lineHeight: '1.3'}}><span>Hi, I'm</span><br></br>
                 <span className="tracking-in-expand">Sebastian</span>
               </h1>
               <h4>I'm a web developer living in NYC.</h4>
-              <h4>Glad you found me! <span id="highFive" style={{paddingLeft: '15px'}}>🙌</span></h4>
+              <h4>Glad you found me! <span id="highFive" style={{paddingLeft: '15px'}} role="img" aria-label="High Five animation">🙌</span></h4>
               <hr></hr>
               <ul className="icons">
                 {socialLinksMarkup}
