1fded1f9ba78bb1fbff2b18d7b5834b185d0b289
scirocco21 <sebgertz@gmail.com>
2019-07-11 08:00:16 -0400

    refactor: use generic Image component and change a tag to button

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,9 @@
-
 import React from 'react';
-// import Img from 'gatsby-image';
 import Image from './image.js'
 import Modal from 'react-awesome-modal';
 import '../assets/sass/project.scss'
 
 export default class Project extends React.Component {
-
   state = {
     visible: false
   }
@@ -20,7 +17,7 @@ export default class Project extends React.Component {
   render() {
     return (    
       <div className="square" onClick={this.toggleModal}>
-        <img src={this.props.imageData}/>
+        <Image imgName={this.props.projectData.imgName} />
         <div id="overlay" style={{color: 'white'}}>
           <div>
             <h3 style={{fontFamily: 'bebas-kai', padding: '5px'}}>{this.props.projectData.name}</h3>
@@ -35,14 +32,26 @@ export default class Project extends React.Component {
         >
           <div>
             <h1 style={{fontSize: '33px'}}>{this.props.projectData.name}</h1>
-            <img src={this.props.imageData} style={{width: '300px', height: '200px'}}/>
+            <Image imgName={this.props.projectData.imgName} />
             <p style={{fontSize: '20px'}}>{this.props.projectData.desc}</p>
-            <button style={{backgroundColor: 'black', color: 'white', marginBottom: '15px', padding: '0 5px 0 5px'}}><h4 style={{margin: '5px 10px 5px 10px', fontFamily: 'bebas-kai', fontSize: '18px'}}><a href={this.props.projectData.url} target="_blank" style={{textDecoration: 'none', color: 'white'}}>View</a></h4></button>
-            <ul class="tags ">
-              {this.props.projectData.tags.map(tag => <li style={{marginBottom: '5px'}}><a href="#" class="tag">{tag}</a></li>)}
+            <button style={{backgroundColor: 'black', color: 'white', marginBottom: '15px', padding: '0 5px 0 5px'}}><h4 style={{margin: '5px 10px 5px 10px', fontFamily: 'bebas-kai', fontSize: '18px'}}><a href={this.props.projectData.url} target="_blank" rel='noopener noreferrer' style={{textDecoration: 'none', color: 'white'}}>View</a></h4></button>
+            <ul className="tags ">
+              {this.props.projectData.tags.map((tag,index) => {
+                return <li 
+                  key={index} 
+                  style={{marginBottom: '5px'}}
+                >
+                  <span className="tag">{tag}</span>
+                </li>
+                })
+              }
             </ul>
-            <a href="javascript:void(0);" onClick={() => this.toggleModal()} style={{position:'absolute',
-  top:'0', right: '10px', textDecoration: 'none'}}><h1 style={{margin: '0', color: 'black'}}>&times;</h1></a>
+            <button
+              onClick={() => this.toggleModal()} 
+              style={{position:'absolute', top:'0', right: '5px', textDecoration: 'none', margin: "0", padding: "0", border: "none"}}
+            >
+              <h1 style={{margin: '0', color: 'black', fontSize: "24px"}}>&times;</h1>
+            </button>
           </div>
         </Modal>
       </div>
