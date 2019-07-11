675f9ce16c940a09c08514c76ed1f3de2c06dce8
scirocco21 <sebgertz@gmail.com>
2019-07-11 08:06:18 -0400

    remove static query

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,38 +1,20 @@
-import React, {useState} from "react"
-import { graphql, StaticQuery } from 'gatsby'
+import React from "react"
 import "../assets/sass/portfolio.scss";
 import Head from '../components/head';
 import ListShuffler from '../components/ListShuffler.js'
 
-class Portfolio extends React.Component {
+class NewPortfolio extends React.Component {
   render() { 
     return(
       <>
         <Head title="Portfolio" />
-        <StaticQuery 
-          query={graphql`
-            query {
-              desktop: file(relativePath: { eq: "museums-explorer.jpg" }) {
-                childImageSharp {
-                  fluid {
-                    ...GatsbyImageSharpFluid_withWebp
-                  }
-                }
-              }
-            }
-          `}
-          render={data => {
-            return (
-              <section style={{padding: '0 40px 40px 40px'}}>
-                <h1 style={{textAlign: 'left'}}>Portfolio</h1>
-                <ListShuffler />
-              </section>
-            ) 
-          }}
-        />
+        <section style={{padding: '0 40px 40px 40px'}}>
+          <h1 style={{textAlign: 'left'}}>Portfolio</h1>
+          <ListShuffler />
+        </section>
       </>
     )
   }
 }
 
-export default Portfolio
+export default NewPortfolio
