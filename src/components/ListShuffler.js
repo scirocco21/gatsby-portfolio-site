9aaadc3693533eca8f7b9076f205101eeda1a7c1
scirocco21 <sebgertz@gmail.com>
2019-07-17 14:12:55 -0400

    update ListShuffler component

--- src/components/ListShuffler.js
+++ src/components/ListShuffler.js
@@ -1,35 +1,23 @@
-import { Flipper, Flipped } from 'react-flip-toolkit'
-import {shuffle} from "lodash";
 import React from "react";
 import Project from './Project.js';
-import config from '../config.js';
+import Fade from 'react-reveal/Fade';
 
  class ListShuffler extends React.Component {
-  state = {
-    data: [1, 2, 3, 4, 5],
-  }
-
-  shuffleList = () => {this.setState({
-    data: shuffle(this.state.data)
-  })}
-
-  componentDidMount() {
-    setTimeout(this.shuffleList, 500);
-  }
-
   render() {
-    return (
-      <Flipper flipKey={this.state.data.join("")}>
+    return (      
+      <Fade bottom>
         <ul className="portfolio-list">
-          {this.state.data.map(d => (
-            <Flipped key={d} flipId={d.toString()}>
-              <li key={d}>
-                <Project toggle={this.toggle} projectData={config.projects[d-1]}/>
+          {this.props.projects.map((project, index) => (
+              <li key={index}>
+                <Project 
+                  projectData={project} 
+                  toggleModal={this.props.toggleModal}
+                  updateProject={this.props.updateProject}
+                />
               </li>
-            </Flipped>
           ))}
         </ul>
-      </Flipper>
+      </Fade>
     )
   }
 };
