e3a7a637067989791358ae9342a07385f4c3d75e
scirocco21 <sebgertz@gmail.com>
2019-07-17 14:13:33 -0400

    bug fix: wrap <li> inside Link component

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -29,28 +29,28 @@ export default class Sidebar extends React.Component {
     const isPartiallyActive = ({ isPartiallyCurrent }) => isPartiallyCurrent ? { className: "nav-item active" } : {className: 'nav-item'};
     const pagesList = config.pages.map((page, index) => {
       return (
-        <li key={index}>
-          <Link 
-            to={`${page.id}`} 
-            className="nav-item" 
-            onClick={this.handleClick} 
-            activeClassName='active' 
-            getProps={ page.id === "/" ? undefined : isPartiallyActive} 
-          >
-            <FontAwesomeIcon icon={page.icon}/>
-            <h5 style={{ margin: '0',
-  padding: '0'}}>{page.title}</h5>
-            <span></span>
-          </Link>
-        </li>
+        <Link 
+          key={index}
+          to={`${page.id}`} 
+          className="nav-item" 
+          onClick={this.handleClick} 
+          activeClassName='active' 
+          getProps={ page.id === "/" ? undefined : isPartiallyActive} 
+        >
+          <li key={index}>
+            <FontAwesomeIcon 
+              icon={page.icon}
+            />
+            <h4 style={{ margin: '0', padding: '0', fontFamily: "bebas-kai", letterSpacing: '0.5px'}}>{page.title}</h4>            
+          </li>
+      </Link>    
       )
     })
     return(
       <aside className={expandedClass}>
-        <header style={{padding: '25px'}}>
+        <header>
           <img 
             src={logo} 
-            style={{ width: '100px'}} 
             alt="Sebastian Gertz Logo"
           />
         </header>
