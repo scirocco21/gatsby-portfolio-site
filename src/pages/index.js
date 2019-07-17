402f6e90d7c8f6efc1e7799757cabb302e1a1f2f
scirocco21 <sebgertz@gmail.com>
2019-07-17 14:25:23 -0400

    refactor: change background image and animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,15 @@ import config from '../config.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Head from "../components/head";
 
-
 export default class Home extends React.Component {
   
   render() {
-    const backgroundImage = "new-background.png"
+    const backgroundImage = "possibe-background.png"
     const socialLinks = config.socialLinks
     const socialLinksMarkup = socialLinks.map(
       link => {
         return <li key={link.id}>
-          <a href={link.url} target='_blank' rel='noopener noreferrer' style={{color: '#48017C'}}>
+          <a href={link.url} target='_blank' rel='noopener noreferrer'>
             <FontAwesomeIcon icon={['fab', link.id]} style={{marginRight: '1.25rem', fontSize: '2rem'}}/>
           </a>
         </li>
@@ -26,13 +25,11 @@ export default class Home extends React.Component {
       <Head title="Home" />
       <div style={{position: 'relative', zIndex: 1}}>
         <BackgroundSection imgName={backgroundImage}>
-          <div id="overlay-main" className='roll-in-left tracking-in-expand'>
+          <div id="overlay-main" className='scale-in-center'>
             <div id="text-container-main">
-              <h1 id="intro-text-main" style={{ fontSize:"2.5rem", lineHeight: '1.3'}}><span>Hi, I'm</span><br></br>
-                <span className="tracking-in-expand">Sebastian</span>
-              </h1>
-              <h4>I'm a web developer living in NYC.</h4>
-              <h4>Glad you found me! <span id="highFive" style={{paddingLeft: '15px'}} role="img" aria-label="High Five animation">🙌</span></h4>
+              <h1 id="intro-text-main" style={{ fontSize:"2.5rem", lineHeight: '1.3'}}>Sebastian Gertz</h1>             
+              <pre style={{fontSize:"2.5rem"}}>{`</>`}</pre>
+              <h4>Full-Stack Web Developer</h4>
               <hr></hr>
               <ul className="icons">
                 {socialLinksMarkup}
@@ -41,7 +38,7 @@ export default class Home extends React.Component {
           </div>
         </BackgroundSection>
       </div>
-      </>
+    </>
     )
   }
 }
