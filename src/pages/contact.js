aa2b24edef2882865ec0d7d96f53ad00e182b97f
scirocco21 <sebgertz@gmail.com>
2019-07-17 14:26:05 -0400

    refactor: change background and animation for contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,11 +3,11 @@ import BackgroundSection from '../components/backgroundImageSection'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../assets/sass/contact.scss'
 
-const backgroundImage = "triangles-download.png"
+const backgroundImage ="soft-background.png"
 
 const Contact = () => ( 
  <BackgroundSection imgName={backgroundImage}> 
-   <div id="form-container"> 
+   <div id="form-container" className="scale-in-center"> 
         <h1>Get in touch</h1>
         <form 
           id="contact-form" 
@@ -27,13 +27,13 @@ const Contact = () => (
               <input name="bot-field"/>
             </label>
           </p>
-          <input  type="text" name="name" maxlength="100" size="60" placeholder="Name"/>
+          <input  type="text" name="name" maxLength="100" size="60" placeholder="Name"/>
           <br/>
-          <input  type="text" name="email" maxlength="100" size="60" placeholder="myname@email.com"/>
+          <input  type="text" name="email" maxLength="100" size="60" placeholder="myname@email.com"/>
           <br/>
-          <input type="text" name="subject" maxlength="200" size="60" placeholder="Subject" />
+          <input type="text" name="subject" maxLength="200" size="60" placeholder="Subject" />
           <br></br>
-          <textarea  name="message" maxlength="2000" cols="6" rows="6" placeholder="What's up? I'm working on..."></textarea>
+          <textarea  name="message" maxLength="2000" cols="6" rows="6" placeholder="What's up? I'm working on..."></textarea>
           <br></br>
           <button style={{margin: '0', padding: '0', backgroundColor: 'rgba(225, 225, 225, 0)', border: 'none'}} type="submit" value="Submit">
             <FontAwesomeIcon icon="paper-plane" style={{ fontSize: '2rem'}}>
