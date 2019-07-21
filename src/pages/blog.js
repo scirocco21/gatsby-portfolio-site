8d612c662e68241e24631f5296acd961fe90c9e5
scirocco21 <sebgertz@gmail.com>
2019-07-20 23:19:31 -0400

    bug fix: wrap <li> in Fade animation

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -31,31 +31,31 @@ const BlogPage = () => {
       <div style={{textAlign: 'left', padding: '0 40px 40px 40px'}}>
         
         <h1>Blog</h1>
-        <Fade bottom>
           <ul style={{listStyle: 'none', padding: '0',margin: '0'}}>
             {data.allContentfulBlogPost.edges.map((edge, index) => {
               return (
-                <li 
-                  style={{marginBottom: '15px'}} 
-                  className="post-card"
-                  key={index}
-                >
-                  <Link to={`/blog/${edge.node.slug}`}>
-                    <div style={{display: 'flex', alignContent: 'center', alignItems: 'center', border: '1px solid #D3D3D3', borderRadius: '5px', boxShadow: '0px 0px 1px 1px #D3D3D3	'}}>
-                      <div style={{padding: '10px 0 10px 10px', height: '100%', minWidth: '33%', width: '33%'}}>
-                        <Img fluid={edge.node.heroImage.fluid} style={{alignSelf: 'flex-start'}}/>
+                <Fade bottom key={index}>
+                  <li 
+                    style={{marginBottom: '15px'}} 
+                    className="post-card"
+                    key={index}
+                  >
+                    <Link to={`/blog/${edge.node.slug}`}>
+                      <div style={{display: 'flex', alignContent: 'center', alignItems: 'center', border: '1px solid #D3D3D3', borderRadius: '5px', boxShadow: '0px 0px 1px 1px #D3D3D3	'}}>
+                        <div style={{padding: '10px 0 10px 10px', height: '100%', minWidth: '33%', width: '33%'}}>
+                          <Img fluid={edge.node.heroImage.fluid} style={{alignSelf: 'flex-start'}}/>
+                        </div>
+                        <div style={{ flex: '1 1 auto', paddingLeft: '10px', paddingRight: '10px'}}>
+                          <h3>{edge.node.title}</h3>
+                          <p>{edge.node.publishedDate}</p>
+                        </div>
                       </div>
-                      <div style={{ flex: '1 1 auto', paddingLeft: '10px', paddingRight: '10px'}}>
-                        <h3>{edge.node.title}</h3>
-                        <p>{edge.node.publishedDate}</p>
-                      </div>
-                    </div>
-                  </Link>
-                </li>
+                    </Link>
+                  </li>
+                </Fade>
               )
             })}
           </ul>
-        </Fade >
       </div>
     </>
   )
