b2ba987ffb8cf99154994e15628ca62fe619bb6b
scirocco21 <sebgertz@gmail.com>
2019-07-26 18:38:04 -0400

    import blog template stylesheet

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -5,6 +5,7 @@ import Img from "gatsby-image";
 import { BLOCKS, MARKS } from '@contentful/rich-text-types';
 import Highlight from 'react-highlight.js'
 import "../assets/monokai.css"
+import "../assets/sass/blog_post.scss"
 
 import {documentToReactComponents} from '@contentful/rich-text-react-renderer';
 export const query = graphql`
