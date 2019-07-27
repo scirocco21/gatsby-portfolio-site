024e6f0127452dac2329b2bf04bf656aed9add5d
scirocco21 <sebgertz@gmail.com>
2019-07-27 02:22:16 -0400

    update blog page to use icons with date and tags

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,6 +4,7 @@ import Img from 'gatsby-image';
 import "../assets/sass/blog.scss"
 import Head from "../components/head"
 import Fade from 'react-reveal/Fade';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -38,22 +39,29 @@ const BlogPage = () => {
                 <Fade bottom key={index}>
                   <li 
                     style={{marginBottom: '15px'}} 
-                    className="post-card"
+                    className="card-container"
                     key={index}
                   >
                     <Link to={`/blog/${edge.node.slug}`}>
-                      <div style={{display: 'flex', alignContent: 'center', alignItems: 'center', border: '1px solid #D3D3D3', borderRadius: '5px', boxShadow: '0px 0px 1px 1px #D3D3D3	'}}>
-                        <div style={{padding: '10px 0 10px 10px', height: '100%', minWidth: '33%', width: '33%'}}>
+                      <div className="blog-card" style={{display: 'flex', alignContent: 'center', alignItems: 'center', border: '1px solid #D3D3D3', borderRadius: '5px', boxShadow: '0px 0px 1px 1px #D3D3D3	'}}>
+                        <div className="image-container">
                           <Img fluid={edge.node.heroImage.fluid} style={{alignSelf: 'flex-start'}}/>
                         </div>
                         <div style={{ flex: '1 1 auto', paddingLeft: '10px', paddingRight: '10px'}}>
                           <h3>{edge.node.title}</h3>
-                          <p>{edge.node.publishedDate}</p>
-                          {edge.node.tag.map(tag => {
-                          return <button style={{backgroundColor: "black", color: "white", padding: "2px 12x 2px 12px", fontSize: "12px"}}>{tag}</button>
-                        })}
+                            <div style={{textAlign: 'left'}}>
+                              <p style={{marginBottom: "5px", marginTop: '5px'}}>
+                                <span style={{marginRight: "10px"}}><FontAwesomeIcon icon="calendar-alt" size="xs"/></span>
+                                {edge.node.publishedDate}
+                              </p>
+                              <p style={{marginTop: "5px", marginBottom: "5px"}}>
+                                <span style={{marginRight: "10px"}}><FontAwesomeIcon icon="tags" size="xs"/></span>
+                                {edge.node.tag.map((tag, index) => {
+                                  return <button key={index} style={{backgroundColor: "black", color: "white", padding: "2px 12x 2px 12px", fontSize: "10px"}}>{tag}</button>
+                                })} 
+                              </p>
+                            </div>
                         </div>
-                       
                       </div>
                     </Link>
                   </li>
