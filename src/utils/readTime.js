5410d7ed94df88e86687a15fdefbaa74dc28baf7
scirocco21 <sebgertz@gmail.com>
2019-08-17 20:27:50 -0400

    feat: add utility for reading time estimate

---
+++ src/utils/readTime.js
@@ -0,0 +1,12 @@
+module.exports = {
+  getReadTime(content) {
+    console.log("I was fired")
+    const pattern = '\\w+';
+    const reg = new RegExp(pattern, 'g');
+    const removables = ['data', 'nodeType', 'value', 'text', 'paragraph', "type", "bold", "code", "heading",'content', 'marks',  'list', 'item', 'italic'];
+    const words = JSON.stringify(content).match(reg);
+    const countableWords = words.filter(word => removables.includes(word) === false);
+    const readTime = Math.ceil(countableWords.length / 125);
+    return `${readTime} - ${readTime + 1} minutes`;
+  }
+}
