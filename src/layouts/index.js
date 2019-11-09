c3d71db9d0352ae9c4a0caccbb563644cabda446
scirocco21 <sebgertz@gmail.com>
2019-11-08 23:31:55 -0500

    import custom font from assets

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,6 +8,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import Footer from './footer.js';
+import '../assets/fonts/BebasKai-Regular.otf'
 
 library.add(fas, fab)
 
