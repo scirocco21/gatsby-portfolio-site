c181d578eef1737356c8a02cb2d5053d265c82cc
scirocco21 <sebgertz@gmail.com>
2019-11-12 14:47:57 -0500

    feat: add hook to toggle dark mode

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { graphql,} from "gatsby";
 import Head from '../components/head'
 import Img from "gatsby-image";
@@ -38,7 +38,6 @@ export const query = graphql`
     }
   } 
 `
-
 const Blog = (props) => {
   const disqusShortname = 'sebastiangertz';
   const disqusConfig = {
@@ -68,61 +67,64 @@ const Blog = (props) => {
       [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
     }
   }
+  const [darkModeOn, toggleDarkMode] = useState(false);
+  const activeClass = darkModeOn? 'dark': 'light';
+
   return (
     <>
       <Head title={props.data.contentfulBlogPost.title} />
-      <label class="switch">
-        <input type="checkbox"/>
-        <span class="slider round"></span>
-      </label>
-      <div style={{padding: '50px', textAlign: 'left'}}>	
-        <div>    
-          <div>        
-            <h1>{props.data.contentfulBlogPost.title}</h1>
-              <p>
-                <FontAwesomeIcon icon="calendar" style={{marginRight: "10px"}}/>{props.data.contentfulBlogPost.publishedDate}
-                <span style={{marginRight: "15px", marginLeft: "15px"}}>/</span>
-                <FontAwesomeIcon icon="calculator" style={{marginRight: "10px"}}/>
-                <CountUp 
-                  end={getWords(props.data.contentfulBlogPost.content.json).length}
-                  delay={0.3}
-                  duration={1.75}
-                  suffix={" words"}
-                />
-                <span style={{marginRight: "15px", marginLeft: "15px"}}>/</span>
-                <FontAwesomeIcon icon="clock" style={{marginRight: "10px"}}/>
-                {getReadTime(getWords(props.data.contentfulBlogPost.content.json))}
-              </p>
-            {props.data.contentfulBlogPost.tldr.internal.content && <h4>TLDR: <em>{props.data.contentfulBlogPost.tldr.internal.content}</em></h4>}
-          </div>
-          <div className="icon-bar">	
-            <FacebookShareButton url={props.location.href} className="facebook">
-              <span className="icon">
-                <FontAwesomeIcon icon={['fab', 'facebook-f']} />
-              </span>
-            </FacebookShareButton>
-            <LinkedinShareButton url={props.location.href} className="linkedin">
-              <span className="icon">
-                <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
-              </span>
-            </LinkedinShareButton>
-            <TwitterShareButton url={props.location.href} className="twitter">
-              <span className="icon">
-                <FontAwesomeIcon icon={['fab', 'twitter']} />
-              </span>
-            </TwitterShareButton>
-          </div>
-        </div>  
-        { 
-          props.data.contentfulBlogPost.heroImage && 
-          <Img 
-            fluid={props.data.contentfulBlogPost.heroImage.fluid} 
-            style={{maxWidth: '70%', height: 'auto'}}
-          />
-        }
-        {documentToReactComponents(props.data.contentfulBlogPost.content.json, options)}
-        <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
-      </div>
+        <label className="switch">
+          <input type="checkbox" onClick={() => toggleDarkMode(!darkModeOn)}/>
+          <span className="slider round"></span>
+        </label>
+        <div className={activeClass} style={{padding: '50px', textAlign: 'left'}}>	
+          <div>    
+            <div>        
+              <h1>{props.data.contentfulBlogPost.title}</h1>
+                <p>
+                  <FontAwesomeIcon icon="calendar" style={{marginRight: "10px"}}/>{props.data.contentfulBlogPost.publishedDate}
+                  <span style={{marginRight: "15px", marginLeft: "15px"}}>/</span>
+                  <FontAwesomeIcon icon="calculator" style={{marginRight: "10px"}}/>
+                  <CountUp 
+                    end={getWords(props.data.contentfulBlogPost.content.json).length}
+                    delay={0.3}
+                    duration={1.75}
+                    suffix={" words"}
+                  />
+                  <span style={{marginRight: "15px", marginLeft: "15px"}}>/</span>
+                  <FontAwesomeIcon icon="clock" style={{marginRight: "10px"}}/>
+                  {getReadTime(getWords(props.data.contentfulBlogPost.content.json))}
+                </p>
+              {props.data.contentfulBlogPost.tldr.internal.content && <h4>TLDR: <em>{props.data.contentfulBlogPost.tldr.internal.content}</em></h4>}
+            </div>
+            <div className="icon-bar">	
+              <FacebookShareButton url={props.location.href} className="facebook">
+                <span className="icon">
+                  <FontAwesomeIcon icon={['fab', 'facebook-f']} />
+                </span>
+              </FacebookShareButton>
+              <LinkedinShareButton url={props.location.href} className="linkedin">
+                <span className="icon">
+                  <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
+                </span>
+              </LinkedinShareButton>
+              <TwitterShareButton url={props.location.href} className="twitter">
+                <span className="icon">
+                  <FontAwesomeIcon icon={['fab', 'twitter']} />
+                </span>
+              </TwitterShareButton>
+            </div>
+          </div>  
+          { 
+            props.data.contentfulBlogPost.heroImage && 
+            <Img 
+              fluid={props.data.contentfulBlogPost.heroImage.fluid} 
+              style={{maxWidth: '70%', height: 'auto'}}
+            />
+          }
+          {documentToReactComponents(props.data.contentfulBlogPost.content.json, options)}
+          <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+        </div>
     </>
   )
 }
