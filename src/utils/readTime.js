4d3d06af40b5bf776e2e89674d956808cbd90abc
scirocco21 <sebgertz@gmail.com>
2019-11-11 19:42:45 -0500

    feat: add getWords method

--- src/utils/readTime.js
+++ src/utils/readTime.js
@@ -1,11 +1,14 @@
 module.exports = {
-  getReadTime(content) {
+  getWords(content) {
     const pattern = '\\w+';
     const reg = new RegExp(pattern, 'g');
     const removables = ['data', 'nodeType', 'value', 'text', 'paragraph', "type", "bold", "code", "heading",'content', 'marks',  'list', 'item', 'italic'];
     const words = JSON.stringify(content).match(reg);
     const countableWords = words.filter(word => removables.includes(word) === false);
-    const readTime = Math.ceil(countableWords.length / 125);
+    return countableWords
+  },
+  getReadTime(words) {
+    const readTime = Math.ceil(words.length / 125);
     return `${readTime} - ${readTime + 1} minutes`;
   }
 }
