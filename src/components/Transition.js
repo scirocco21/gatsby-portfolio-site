1d167e5d999e33ef73a5b37eaedbc697d87e5745
scirocco21 <sebgertz@gmail.com>
2019-11-17 17:55:14 -0500

    feat: add transition component for smoother page transitions

---
+++ src/components/Transition.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { TransitionGroup, Transition as ReactTransition } from 'react-transition-group';
+
+// The duration for each phase of the transition
+// So the total duration will be _twice_ this
+const timeout = 150;
+
+const styles = {
+  entering: {
+    position: 'absolute',
+    opacity: 0,
+  },
+  entered: {
+    transition: `opacity ${timeout}ms ease`,
+    opacity: 1,
+  },
+  exiting: {
+    transition: `opacity ${timeout}ms ease`,
+    opacity: 0
+  },
+};
+
+const Transition = ({ children, location }) => (
+  <TransitionGroup>
+    <ReactTransition key={location.pathname} timeout={timeout}>
+      {(status) => (
+        <div style={styles[status]}>
+          {children}
+        </div>
+      )}
+    </ReactTransition>
+  </TransitionGroup>
+);
+
+export default Transition;
