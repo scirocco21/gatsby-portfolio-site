d37b6635eeb3448ec1d747ca44ae24e35cd48ad7
scirocco21 <sebgertz@gmail.com>
2019-11-18 21:49:01 -0500

    feat: add site-wide dark mode option

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,24 +10,46 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 import Footer from './footer.js';
 import '../assets/fonts/BebasKai-Regular.otf'
 import Transition from '../components/Transition';
-
+import DarkModeToggler from '../components/DarkModeToggler';
+import '../assets/sass/dark_mode_toggler.scss'
 
 library.add(fas, fab)
 
 class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      darkModeOn: false
+    }
+  }
+  toggleDarkMode = () => {
+    this.setState({
+      darkModeOn: !this.state.darkModeOn
+    })
+  }
   render() {
+    let activeClass = this.state.darkModeOn? 'dark': 'light';
     return(
       <>
         <div id="main-container" style={{position: "relative"}}>
-        <div style={{position: "absolute", width: "100%", height: "100%"}}>
-          <NavBar siteTitle={this.props.title}/>
-        </div>
+        
+
+          <div style={{position: "absolute", width: "100%", height: "100%"}}>
+            <NavBar siteTitle={this.props.title}/>
+          </div>
           <div style={{position: "absolute", height: "100%"}}>
             <Sidebar siteTitle={this.props.title}/>
           </div>
+
           <Transition location={this.props.location}>
+          
             <div className="container">
-              <main style={{ flex: 1 }}>{this.props.children}</main>
+              <main style={{ flex: 1 }} className={activeClass}>
+                {this.props.location.pathname !== "/" 
+                && this.props.location.pathname !== "/contact"
+                && <DarkModeToggler toggleDarkMode={this.toggleDarkMode}/>}
+                {React.cloneElement(this.props.children, { darkModeOn: this.state.darkModeOn })}
+              </main>
               { this.props.location.pathname !== "/" && <Footer /> } 
             </div>
           </Transition>
