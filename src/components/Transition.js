edb3a070602f0ae6b8ae4c5ea0ab5b3d32aecc33
scirocco21 <sebgertz@gmail.com>
2020-05-03 15:47:44 -0400

    UI fix: change transition from opacity to transform

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { TransitionGroup, Transition as ReactTransition } from 'react-transition-group';
-
 // The duration for each phase of the transition
 // So the total duration will be _twice_ this
-const timeout = 250;
+const timeout = 200;
 
 const styles = {
   entering: {
     position: 'absolute',
-    opacity: 0,
+    transform: 'rotate(360deg)',
   },
   entered: {
-    transition: `opacity ${timeout}ms ease`,
-    opacity: 1,
+    transition: `transform ${timeout}ms ease`
   },
   exiting: {
-    transition: `opacity ${timeout}ms ease`,
-    opacity: 0
-  },
+    transition: `transform ${timeout}ms ease`,
+  }
 };
 
 const Transition = ({ children, location }) => (
