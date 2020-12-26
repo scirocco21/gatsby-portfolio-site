93a7fab1a5f6edd0e2528c27b427326d84ed8e9e
scirocco21 <sebgertz@gmail.com>
2020-12-25 22:22:46 -0500

    change back to simpler fade transition

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -2,19 +2,13 @@ import React from 'react';
 import { TransitionGroup, Transition as ReactTransition } from 'react-transition-group';
 // The duration for each phase of the transition
 // So the total duration will be _twice_ this
-const timeout = 120;
+const timeout = 150;
 
 const styles = {
-  entering: {
-    position: 'absolute',
-    transform: 'rotate(90deg)',
-  },
-  entered: {
-    transition: `transform ${timeout}ms ease-out`
-  },
-  exiting: {
-    transition: `transform ${timeout}ms ease-out`,
-  }
+  entering: { opacity: 0.5 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0.5 },
+  exited: { opacity: 0 }
 };
 
 const Transition = ({ children, location }) => (
